2909ee1206e5a6d946826600b525f6ff544b25b5
marsh5 <brodiemarshall567@gmaill.com>
2020-12-16 22:41:19 -0500

    added react components that fetch data from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React, { Fragment } from 'react';
+import InputTodo from './components/InputTodo';
+import ListTodos from './components/ListTodos';
 
 function App() {
+  
   return (
    <Fragment>
-     
+     <div className="container">
+     <InputTodo />
+     <ListTodos />
+     </div>
    </Fragment>
   );
 }
